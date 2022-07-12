15d32d8c5f2b38c7af8758c065ea698ba43ee583
vany <79766479+vanybhops@users.noreply.github.com>
2022-07-12 19:27:39 +0200

    Update paste to console.js

--- paste to console.js
+++ paste to console.js
@@ -56,7 +56,6 @@ async function ws(token) {
     let custom="kastm kod"
     let kastm=false
     let exits=true;
-    let bl="947598329498443896/996396961773125752"
     async function kastommeni(){
         let kastmmenu = document.createElement("div");
         document.body.appendChild(kastmmenu)
